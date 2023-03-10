7cc620f2801d4c42adad3d3b2fa0a6c03096ec7f
Daniel Hines <d4hines@gmail.com>
2023-03-10 16:26:00 -0500

    broken nix flake environment

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(builtins.trace rust-overlay.overlay rust-overlay.overlays.default) ];
+      };
+    in {
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+          alejandra
+          rustfmt
+          rust-analyzer
+          wabt
+          clang
+
+          (rust-bin.stable."1.66.0".default.override {
+            targets = ["wasm32-unknown-unknown"];
+          })
+        ];
+      };
+    });
+}
